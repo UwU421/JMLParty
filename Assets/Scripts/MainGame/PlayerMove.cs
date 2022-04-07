b068afcb4e391ebe1a9283f30bf4b7bd6c0d2024
UwU <uwu4212@gmail.com>
2022-04-07 09:15:13 +0200

    jamal

---
+++ Assets/Scripts/MainGame/PlayerMove.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class PlayerMove : MonoBehaviour
+{
+
+    public float speedX = 2;
+    public float speedY = 2;
+
+    PhotonView view;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        view = GetComponent<PhotonView>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(view.IsMine)
+        {
+            float inputX = Input.GetAxis("Horizontal");
+            float inputY = Input.GetAxis("Vertical");
+            Vector3 movement = new Vector3(speedX * inputX, speedY * inputY, 0);
+            movement *= Time.deltaTime;
+            transform.Translate(movement);
+        }
+    }
+}
---
+++ Assets/Scripts/MainGame/SpawnPlayers.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class SpawnPlayers : MonoBehaviour
+{
+    
+    public GameObject playerObj;
+
+    public float minX;
+    public float minY;
+    public float maxX;
+    public float maxY;
+
+    private void Start()
+    {
+        Vector3 randomPosition = new Vector3(Random.Range(minX,maxX),Random.Range(minY,maxY),0);
+        PhotonNetwork.Instantiate(playerObj.name, randomPosition, Quaternion.identity);
+    }
+
+}
